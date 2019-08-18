900dbc9c4e9ee611ff19a2b6ce0c9fc534c551a3
VTC <vtchris@hotmail.com>
2019-08-18 18:54:57 -0400

    article/note models added, early handlebars, changed news source to npr

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,14 +5,62 @@ var mongoose = require("mongoose");
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/newsScrapper", { useNewUrlParser: true });
 
+// Require all models
+var db = require("../models");
+
 module.exports = function (app) {
     // Root get route
     app.get("/", function (req, res) {
 
-    })
+        var articles = []
+
+        axios.get("https://www.npr.org/sections/news/").then(function (response) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(response.data);
+            
+            
+            // Now, find the articles
+            $("article").each(function (i, element) {
+                // Save an empty result object
+                var result = {};
+                var summary = $(this).find("p.teaser").find("a").text()
+                summary = summary.split("• ")
+               
+                result.title = $(this)
+                    .find("h2").find("a")
+                    .text();
+                result.summary = summary[1]
+                result.link = $(this)
+                    .find("h2")
+                    .find("a")
+                    .attr("href");
+                result.date_published = $(this)
+                    .find("time")
+                    .attr("datetime")
+
+                if (result.link && result.summary && result.title) {
+                    articles.push(result)
+
+                }
+            });
+
+            // Send a message to the client 
+       
+            //res.send("Scrape Complete");
+        }).then(function(){
+            res.render("index",{articles: articles})
+        })
+        
+
+    });
+
+
+
+
     app.get("/scrape", function (req, res) {
         // First, we grab the body of the html with axios
-        axios.get("https://www.nytimes.com/section/world/").then(function (response) {
+        //axios.get("https://www.nytimes.com/section/world/").then(function (response) {
+        axios.get("https://www.npr.org/sections/news/").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
@@ -25,28 +73,31 @@ module.exports = function (app) {
                     .find("h2").find("a")
                     .text();
                 result.summary = $(this)
-                    .find("p")
+                    .find("p.teaser")
+                    .find("a")
                     .text();
                 result.link = $(this)
                     .find("h2")
                     .find("a")
                     .attr("href");
+                result.date_published = $(this)
+                    .find("time")
+                    .attr("datetime")
 
                 if (result.link && result.summary && result.title) {
-                    console.log(result)
+
+                    //Create a new Article using the `result` object built from scraping
+                    db.Article.create(result)
+                        .then(function (dbArticle) {
+                            // View the added result in the console
+                            console.log(dbArticle);
+                        })
+                        .catch(function (err) {
+                            if (err.code !== 11000) {
+                                console.log(err);
+                            }
+                        });
                 }
-
-
-                // Create a new Article using the `result` object built from scraping
-                // db.Article.create(result)
-                //   .then(function(dbArticle) {
-                //     // View the added result in the console
-                //     console.log(dbArticle);
-                //   })
-                //   .catch(function(err) {
-                //     // If an error occurred, log it
-                //     console.log(err);
-                //   });
             });
 
             // Send a message to the client
