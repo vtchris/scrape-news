c906eb9720fe456141e4218f3e9cffac61423fc6
VTC <vtchris@hotmail.com>
2019-08-19 11:01:07 -0400

    Added routing for saving articles

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,101 +9,69 @@ mongoose.connect("mongodb://localhost/newsScrapper", { useNewUrlParser: true });
 var db = require("../models");
 
 module.exports = function (app) {
+
+    var articles = []
     // Root get route
     app.get("/", function (req, res) {
-
-        var articles = []
 
         axios.get("https://www.npr.org/sections/news/").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
-            
-            
+
             // Now, find the articles
             $("article").each(function (i, element) {
                 // Save an empty result object
                 var result = {};
                 var summary = $(this).find("p.teaser").find("a").text()
                 summary = summary.split("• ")
-               
+                var link = $(this).find("h2").find("a").attr("href");               
+
                 result.title = $(this)
                     .find("h2").find("a")
                     .text();
-                result.summary = summary[1]
-                result.link = $(this)
-                    .find("h2")
-                    .find("a")
-                    .attr("href");
+                result.summary = summary[1];
+                result.link = link; 
                 result.date_published = $(this)
                     .find("time")
-                    .attr("datetime")
+                    .attr("datetime");
+
+                
 
                 if (result.link && result.summary && result.title) {
+                    //Get article id from the link
+                    var linkParts = link.split("/")
+                    result.id = linkParts[linkParts.length-2];
+                    
                     articles.push(result)
 
                 }
             });
-
-            // Send a message to the client 
-       
-            //res.send("Scrape Complete");
-        }).then(function(){
-            res.render("index",{articles: articles})
+     
+        }).then(function () {
+            res.render("index", { articles: articles })
         })
-        
-
     });
 
+    // Save an article
+    app.post("/articles/:id", function (req, res) {
 
-
-
-    app.get("/scrape", function (req, res) {
-        // First, we grab the body of the html with axios
-        //axios.get("https://www.nytimes.com/section/world/").then(function (response) {
-        axios.get("https://www.npr.org/sections/news/").then(function (response) {
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
-            var $ = cheerio.load(response.data);
-
-            // Now, find the articles
-            $("article").each(function (i, element) {
-                // Save an empty result object
-                var result = {};
-
-                result.title = $(this)
-                    .find("h2").find("a")
-                    .text();
-                result.summary = $(this)
-                    .find("p.teaser")
-                    .find("a")
-                    .text();
-                result.link = $(this)
-                    .find("h2")
-                    .find("a")
-                    .attr("href");
-                result.date_published = $(this)
-                    .find("time")
-                    .attr("datetime")
-
-                if (result.link && result.summary && result.title) {
-
-                    //Create a new Article using the `result` object built from scraping
-                    db.Article.create(result)
-                        .then(function (dbArticle) {
-                            // View the added result in the console
-                            console.log(dbArticle);
-                        })
-                        .catch(function (err) {
-                            if (err.code !== 11000) {
-                                console.log(err);
-                            }
-                        });
+        // Find the article in the array based on the id
+        var id = req.params.id
+        const article = articles.find( article => article.id === id );
+        
+        // Save new record using the article
+        db.Article.create(article)
+            .then(function (dbArticle) {
+                // View the added result in the console
+                console.log(dbArticle);
+                res.json({"success": true})
+            })
+            .catch(function (err) {
+                // Suppress duplicate errors 
+                if (err.code !== 11000) {
+                    console.log(err);
                 }
             });
-
-            // Send a message to the client
-            res.send("Scrape Complete");
-        });
-    });
-
+    })
 
 }
