1f820c1f689c2de373f62fd69b9aafc2781ded89
VTC <vtchris@hotmail.com>
2019-08-21 20:59:06 -0400

    Added note functionality, add/delete

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,54 @@
+// DOM variables
 const $saveNote = $("#save-note-btn");
 const $notesModal = $("#notes-modal");
+const $notesTitle = $("#notes-title");
+const $notesUL = $("#notes-ul");
 
+$(".js_addNote").on("click", function (e) {
+
+  e.preventDefault();
+
+  const id = $(this).parent().attr("data-id");
+
+  $.get("/articles/" + id).then(function (res) {
+
+    $notesUL.empty();
+    const notes = res.note
+
+    $notesTitle.text(res.title);
+
+    for (let i = 0; i < notes.length; i++) {
+      let li = $("<li>");
+      let btn = $("<button>");
+      li.addClass("d-flex mb-2")
+      li.text(notes[i].text);
+      btn.addClass("btn btn-sm btn-danger mr-2 js_deleteNote")
+      btn.html("<span class='fa fa-window-close'></span>")
+      btn.attr("id", notes[i]._id)
+      btn.prependTo(li)
+
+      //Append new li
+      li.appendTo($notesUL);
+    }
+    //Add click handlers to new delete buttons
+    $(".js_deleteNote").on("click", function (e) {
+    
+      e.preventDefault();
+    
+      $.ajax({
+        url: "/notes/" + this.id,
+        method: "DELETE"
+      }).then(res, function () {
+        console.log(res)
+      })
+    
+    })
+
+    $saveNote.attr("data-id", id);
+    $notesModal.modal("show");
+
+  })
+})
 $(".js_saveArticle").on("click", function (e) {
 
   e.preventDefault();
@@ -12,29 +60,21 @@ $(".js_saveArticle").on("click", function (e) {
   $.post("/articles/" + id).then(function (res) {
     console.log(res)
   })
-
 });
-$(".js_addNote").on("click", function (e) {
-
-  e.preventDefault();
-
-  $saveNote.attr("data-id", $(this).parent().attr("data-id"));
-  $notesModal.modal("show");
-
-})
 $("#save-note-btn").on("click", function (e) {
 
   e.preventDefault();
 
   const articleId = $(this).attr("data-id")
- 
+
   $.ajax({
     method: "POST",
     url: "/notes/" + articleId,
     data: {
       // Value taken from title input
       //title: $("#titleinput").val(),  
-      body: $("#note-txt").val()
+      articleId: articleId,
+      text: $("#note-txt").val()
     }
   })
     // With that done
@@ -44,8 +84,5 @@ $("#save-note-btn").on("click", function (e) {
       // Empty the notes section
       $("#note-txt").empty();
     });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  //   $("#titleinput").val("");
-  //   $("#bodyinput").val("");
+ 
 })
